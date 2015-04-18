dbbd24e7c5256227c0f11e6b7e4e94800fac7b6a
Codu0409 <corentindumont@hotmail.com>
2015-04-19 00:56:08 +0200

    add categoriesSelection , the user can choose his categories after sign up or is redirected to the mainPage

---
+++ BlankCordovaApp1/scripts/categoriesSelection.js
@@ -0,0 +1,132 @@
+﻿/// <reference group="Dedicated Worker" />
+
+
+$(document).ready(function () {
+
+    var data;
+
+    var mailUser = window.localStorage.getItem("mailUser");
+
+    var $email = window.localStorage.getItem("mailUser");
+
+    var $jsonFormat = {
+        Email: $email,
+    };
+
+    var allCategories = new Array();
+    var idCategories = new Array();
+    var index = 0;
+    var isFinished = false;
+
+    $.getJSON("http://shareamoment.azurewebsites.net/api/Categories", function (data) {
+
+        var jsonData = JSON.parse(data);
+        var ul = document.getElementById("activityInfosList");
+
+        $.each(jsonData, function (idx, obj) {
+
+            allCategories.push(obj.Name);
+            idCategories.push(obj.CategoryId);
+        });
+
+
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(allCategories[index]));
+        li.setAttribute("id", allCategories[index]);
+        ul.appendChild(li);
+    });
+
+    $("#buttonYes").click(function () {
+
+        var $email = window.localStorage.getItem("mailUser");
+        var $activityID = idCategories[index];
+
+        if (isFinished) {
+            window.localStorage.setItem("mailUser", $email);
+            document.location.href = "mainPage.html";
+        }
+
+        if (index == allCategories.length-2) {
+            document.getElementById("buttonYes").innerHTML = "Accueil";
+            isFinished = true;           
+        }
+
+        var $jsonFormat = { UserEmail: $email, idCategory: $activityID };
+        
+        $.ajax({
+            type: "POST",
+            url: "http://shareamoment.azurewebsites.net/api/Users/Updateusercategory",
+            data: JSON.stringify($jsonFormat),
+            dataType: "json",
+            contentType: "application/json",
+            async: false,
+        }).done(function (data) {
+
+
+
+        }).fail(function (data) {
+            Success = true;
+        })
+
+
+
+        var li = document.getElementById(allCategories[index]);
+        if (index < allCategories.length-1)
+        {
+            index = index + 1;
+        }
+        
+        
+        li.innerHTML = allCategories[index];
+        li.setAttribute("id", allCategories[index]);
+    });
+
+    $("#buttonNo").click(function () {
+        var $email = window.localStorage.getItem("mailUser");
+        var $activityID = idCategories[index];
+
+        if (isFinished) {
+            window.localStorage.setItem("mailUser", $email);
+            document.location.href = "mainPage.html";
+        }
+
+        if (index == allCategories.length - 2) {
+            document.getElementById("buttonNo").innerHTML = "Accueil";
+            isFinished = true;
+        }
+        var li = document.getElementById(allCategories[index]);
+        if (index < allCategories.length - 1) {
+            index = index + 1;
+        }
+        li.innerHTML = allCategories[index];
+        li.setAttribute("id", allCategories[index]);
+    });
+
+
+
+    //$.ajax({
+    //    type: "POST",
+    //    url: "http://shareamoment.azurewebsites.net/api/Categories",
+    //    data: JSON.stringify($jsonFormat),
+    //    dataType: "json",
+    //    contentType: "application/json",
+    //    async: false,
+    //}).done(function (data) {
+    //    $.each(data, function (idx, obj) {
+    //        allCategories[allCategories.length] = obj.Name;
+    //    });
+    //    var index = 0;
+    //    var ul = document.getElementById("activityInfosList");
+    //    var li = document.createElement("li");
+    //    li.appendChild(document.createTextNode(allCategories[index]));
+    //    li.setAttribute("id", allCategories[index]);
+    //    ul.appendChild(li);
+
+    //}).fail(function (data) {
+    //    Success = true;
+    //})
+
+
+    
+
+});
