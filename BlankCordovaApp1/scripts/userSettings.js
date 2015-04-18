942786edb71f5ef7d5f32bf3b7682494a4fc631d
Codu0409 <corentindumont@hotmail.com>
2015-04-18 14:56:46 +0200

    update , class css color to compare

--- BlankCordovaApp1/scripts/userSettings.js
+++ BlankCordovaApp1/scripts/userSettings.js
@@ -8,27 +8,44 @@ $(document).ready(function () {
 
     var data;
 
-
+    //Récupération de toutes les catégories.
     $.getJSON("http://shareamoment.azurewebsites.net/api/Categories", function (data) {
 
         var jsonData = JSON.parse(data);
         var ul = document.getElementById("activitiesList");
-        var i = 0;
+
         $.each(jsonData, function (idx, obj) {
 
 
             var li = document.createElement("li");
             li.appendChild(document.createTextNode(obj.Name));
-            li.setAttribute("id", "categorie" + i);
-            li.setAttribute("class", "isSelected");
-            i = i + 1;
+
+
+
+            li.setAttribute("id", obj.CategoryId);
+            li.setAttribute("class", "isNotSelected");
+
+
+
             ul.appendChild(li);
+            
         });
+
+        $('li').click(function () {
+            
+            //if ($(this).css().val)
+            $(this).setAttribute("class", "isNotSelected");
+            alert($(this).css("background-color"));
+            if ($(this).css("background-color") == "rgb(63,246,34)")
+            {
+                alert("wesh");
+            }
+                
+
+        })
     });
 
-    $("li").click(function () {
-        alert("yo");
-    })
+    
        
         
    
